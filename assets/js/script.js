545dad6f9b5854e62b402dd31320e9413c3af16f
nic-d <nicanthonidiaz@gmail.com>
2023-09-14 15:46:57 -0400

    psuedo code added, and base for HTML file added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,21 @@
 // PSUEDO CODE:
-// 
+// create variables / arrays /objects
+// timer variable
+// create objects that hold all the questions + possible answers
+// use document.queryselector to store the btn div (asign id to btn) as a variable = btn
+// create startQuiz function that is triggered by 'click' event lastener, attached to the button variable
+// startquiz() should hold a var = timeInterval, which is = setInterval(function ()
 //
 //
-// 
 //
 //
 //
-//
-//
-//
-//
+// call startQuiz();
+
+time = 100;
+initials = '';
+score = '';
+
+allQuestions = {
+    
+}
