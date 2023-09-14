cd8554f914906bb96f6abe0046748b39497a2e1d
nic-d <nicanthonidiaz@gmail.com>
2023-09-14 16:51:12 -0400

    added html additional html and css - base almost done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 // PSUEDO CODE:
-// create variables / arrays /objects
-// timer variable
-// create objects that hold all the questions + possible answers
+// create variables / arrays / objects
+// create object that hold all the questions + possible answers
 // use document.queryselector to store the btn div (asign id to btn) as a variable = btn
 // create startQuiz function that is triggered by 'click' event lastener, attached to the button variable
 // startquiz() should hold a var = timeInterval, which is = setInterval(function ()
@@ -14,8 +13,57 @@
 
 time = 100;
 initials = '';
-score = '';
+score = 0;
+//objects holding mult coice questions
+var questions = [
+    {
+        pompt: "This will be question 1?\n(a) A1\n(b) A2\n(c) A3\n(d) A4",
+        answer: "b"
+    },
+    {
+        pompt: "This will be question 2?\n(a) A1\n(b) A2\n(c) A3\n(d) A4",
+        answer: "d"
+    },
+    {
+        pompt: "This will be question 3?\n(a) A1\n(b) A2\n(c) A3\n(d) A4",
+        answer: "d"
+    },
+    {
+        pompt: "This will be question 4?\n(a) A1\n(b) A2\n(c) A3\n(d) A4",
+        answer: "c"
+    },
+    {
+        pompt: "This will be question 5?\n(a) A1\n(b) A2\n(c) A3\n(d) A4",
+        answer: "a"
+    },
+    {
+        pompt: "This will be question 6?\n(a) A1\n(b) A2\n(c) A3\n(d) A4",
+        answer: "b"
+    },
+    {
+        pompt: "This will be question 7?\n(a) A1\n(b) A2\n(c) A3\n(d) A4",
+        answer: "a"
+    },
+    {
+        pompt: "This will be question 8?\n(a) A1\n(b) A2\n(c) A3\n(d) A4",
+        answer: "a"
+    },
+    {
+        pompt: "This will be question 9?\n(a) A1\n(b) A2\n(c) A3\n(d) A4",
+        answer: "c"
+    },
+    {
+        pompt: "This will be question 10?\n(a) A1\n(b) A2\n(c) A3\n(d) A4",
+        answer: "d"
+    },
 
-allQuestions = {
-    
+]
+
+for (i = 0; i < questions.length; i++;) {
+    var answer = //addeventlistener that stores which element the user clicks on as the 'answer'
+   if (answer == questions[i].answer) {
+        score++;
+    } else (
+        time == time - 10; // doing this in the case of 10 total questions 
+   )
 }
