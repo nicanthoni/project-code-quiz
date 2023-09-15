2aa9e60b892b7c0646571f1f77800139b51ec234
nic-d <nicanthonidiaz@gmail.com>
2023-09-15 18:33:42 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ var startBtn = document.querySelector(".start-btn");
 var mainContainer = document.querySelector(".main-container");
 var timer = document.querySelector(".timer");
 var viewScores = document.querySelector('.view-scores');
-var answerOptions = document.getElementsByClassName('.a-btn'); // array, since selecting ALL the options
+var answerOptions = document.getElementsByClassName('a-btn'); // array, since selecting ALL the options
 var displayedQuestion = document.querySelector('#question');
 var currentQuestion = 0; // to track current question, bc the 1st index of an array is 0, which is whats holding the questions.then increase it each time I get the next question by 1
 
@@ -78,11 +78,7 @@ function startQuiz() {
 // Getting all the questions, and index will display the first one
 let index = 0;
 function getQuestion() {
-    for (let i = 0; i < questions.length; i++) {
-        console.log(questions[i].question);
-        
-        displayedQuestion.textContent = questions[index].question;
-    }
+    displayedQuestion.innerText = questions[currentQuestion].question;
 }
 
 //  loop to get all the options/choices -> assign the event listener to each and check if the answer is correct and move to the next question with index++ to display 1 by 1
@@ -90,20 +86,19 @@ function getChoices() {
     for (let i = 0; i < 4; i++) {
         console.log(questions[i].choices[i]);
 
-        answerOptions[i].textContent = questions[index].answer;
+        answerOptions[i].innerText = questions[currentQuestion].choices[i];
     }
 }
 
 
-// function checkAnswers(event) { // event listener runs when answer btn clicked -> check if event.target === the questions.answer
-//     if (event.target.value === questions.answer.value) {
-//         index++;
-//         score++;
-//     } else {
-
-//     }
-
-// };
+function checkAnswers(event) { // event listener runs when answer btn clicked -> check if event.target === the questions.answer
+    if (event.target.value === questions.answer.value) {
+        currentQuestion++;
+        score++;
+        getQuestion();
+        getChoices();
+    } else return;
+};
 
 function startTimer() {
 };
@@ -114,6 +109,6 @@ function viewHighScores() {
 
 
 startBtn.addEventListener("click", startQuiz); // starts quiz when start button is clicked
-// answerOptions.addEventListener('click', checkAnswers); // listen for click of the answer options, and run the function that compares to the correct answer, if statement for what happens if matches correct answer or not
+answerOptions.addEventListener('click', checkAnswers); // listen for click of the answer options, and run the function that compares to the correct answer, if statement for what happens if matches correct answer or not
 
 // startQuiz();
